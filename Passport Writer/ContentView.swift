b6fdc09e9107a2ade69266504959ea7231f861a6
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-24 03:36:10 -0500

    Initial Commit

---
+++ Passport Writer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Passport Writer
+//
+//  Created by Matthew Stanciu on 2/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Passport Writer/Passport_WriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Passport_WriterApp.swift
+//  Passport Writer
+//
+//  Created by Matthew Stanciu on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Passport_WriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
