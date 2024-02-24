b3c9e47442e1b70e5979724b12ad87b7b3f97943
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-24 03:39:59 -0500

    add basic list

--- Passport Writer/ContentView.swift
+++ Passport Writer/ContentView.swift
@@ -6,6 +6,45 @@
 //
 
 import SwiftUI
+struct Passport: Identifiable {
+    let name: String
+    let surname: String
+    let id: Int32
+    let secret: String
+}
+
+/// these secrets are fake, just for testing
+var passports = [
+    Passport(name: "Jack", surname: "Hogan", id: 1, secret: "2eV78TKypsBGgNZG7aX"),
+    Passport(name: "Matthew", surname: "Stanciu", id: 12, secret: "Ot935wO9KEnV4fdFLGLxl")
+]
+
+struct PersonRowView: View {
+    var passport: Passport
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 3) {
+            Text(passport.name + " " + passport.surname)
+                .foregroundColor(.primary)
+                .font(.headline)
+            HStack(spacing: 3) {
+                Label(String(passport.id), systemImage: "person.text.rectangle.fill")
+            }
+            .foregroundColor(.secondary)
+            .font(.subheadline)
+        }
+    }
+}
+
+struct StaffList: View {
+    var body: some View {
+        List {
+            ForEach(passports) { person in
+                PersonRowView(passport: person)
+            }
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
@@ -14,11 +53,12 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .foregroundColor(.green)
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    StaffList()
 }
